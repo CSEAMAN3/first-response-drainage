2319f17be1466194bf2cc85dc269b9f77af2c6d9
Christopher Seaman <christopherseaman1983@gmail.com>
2026-01-30 10:29:08 +0000

    update blog image classes to fix object cover

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -61,7 +61,7 @@ export default async function BlogPage() {
                       <CloudinaryUnOptImage
                         {...img}
                         alt={coverImageAlt ?? img.alt}
-                        className="object-cover rounded-sm group-hover:brightness-75 transition-all duration-300"
+                        className="w-full h-full object-cover rounded-sm group-hover:brightness-75 transition-all duration-300"
                       />
                     </div>
                     <div className="grid grid-cols-1">
